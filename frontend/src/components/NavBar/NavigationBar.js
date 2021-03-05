4c0c120c65cc71115c62aa966fa171abdbd4e2d5
Mick <micklwl@hotmail.com>
2021-03-05 16:21:45 +0800

    fix log out

--- frontend/src/components/NavBar/NavigationBar.js
+++ frontend/src/components/NavBar/NavigationBar.js
@@ -12,7 +12,7 @@ export const NavigationBar = () => {
     const history = useHistory();
 
      const test = () => {
-         actions.logout();
+         actions.thunkLogout();
          history.push("/");
      }
 
